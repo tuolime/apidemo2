d73046cf376aa1155b81f3abe0f5b73bc22ce95d
guojinlong <guojinlong@norinaviation.com>
2023-08-01 18:02:05 +0800

    工作一：部分逻辑

--- app/src/main/java/com/ss/apidemo/ui/mode/WorkSelectOneActivity.java
+++ app/src/main/java/com/ss/apidemo/ui/mode/WorkSelectOneActivity.java
@@ -2,17 +2,118 @@ package com.ss.apidemo.ui.mode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.king.view.arcseekbar.ArcSeekBar;
+import com.ss.apidemo.AppConfig;
+import com.ss.apidemo.MyApplication;
 import com.ss.apidemo.R;
 import com.ss.apidemo.base.BaseActivity;
+import com.ss.apidemo.ui.SplashActivity;
+import com.ss.apidemo.utils.PlayVoiceUtils;
 
 public class WorkSelectOneActivity extends BaseActivity {
+
+    private TextView fan_1,fan_2,fan_3,fan_4,fan_5;
+    private TextView tv_fluence;
+    private ArcSeekBar arcSeekBar;
+    private int fan_flag = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work_select_one);
+        initView();
+        initData();
+    }
+
+    private void initView() {
+        fan_1 = findViewById(R.id.fan_1);
+        fan_2 = findViewById(R.id.fan_2);
+        fan_3 = findViewById(R.id.fan_3);
+        fan_4 = findViewById(R.id.fan_4);
+        fan_5 = findViewById(R.id.fan_5);
+        arcSeekBar = findViewById(R.id.arcSeekBar);
+        tv_fluence = findViewById(R.id.tv_fluence);
+
+    }
+    private void initData() {
+
+    }
+
+    public void tabClick(View view) {
+        PlayVoiceUtils.startPlayVoice(MyApplication.instance(), AppConfig.KEY);
+        switch (view.getId()) {
+            case R.id.iv_save:
+                break;
+            case R.id.iv_setting:
+                break;
+            case R.id.iv_back:
+                finish();
+                break;
+            case R.id.iv_main:
+                startActivity(new Intent(WorkSelectOneActivity.this, SplashActivity.class));
+                break;
+        }
+    }
+
+    public void modeClick(View view) {
+        PlayVoiceUtils.startPlayVoice(MyApplication.instance(), AppConfig.KEY);
+        switch (view.getId()) {
+            case R.id.iv_save:
+                break;
+            case R.id.iv_setting:
+                break;
+            case R.id.iv_back:
+                finish();
+                break;
+            case R.id.iv_main:
+                startActivity(new Intent(WorkSelectOneActivity.this, SplashActivity.class));
+                break;
+        }
+    }
+    public void selectClick(View view) {
+        PlayVoiceUtils.startPlayVoice(MyApplication.instance(), AppConfig.KEY);
+        resetMenuState();
+        switch (view.getId()) {
+            case R.id.iv_fan:
+                fan_flag++;
+                if (fan_flag == 1){
+                    fan_1.setBackground(getResources().getDrawable(R.drawable.work_one_fan_1_select_corners));
+                }if (fan_flag == 2){
+                    fan_1.setBackground(getResources().getDrawable(R.drawable.work_one_fan_1_select_corners));
+                    fan_2.setBackground(getResources().getDrawable(R.drawable.work_one_fan_2_select_corners));
+                }if (fan_flag == 3){
+                    fan_1.setBackground(getResources().getDrawable(R.drawable.work_one_fan_1_select_corners));
+                    fan_2.setBackground(getResources().getDrawable(R.drawable.work_one_fan_2_select_corners));
+                    fan_3.setBackground(getResources().getDrawable(R.drawable.work_one_fan_2_select_corners));
+                }if (fan_flag == 4){
+                    fan_1.setBackground(getResources().getDrawable(R.drawable.work_one_fan_1_select_corners));
+                    fan_2.setBackground(getResources().getDrawable(R.drawable.work_one_fan_2_select_corners));
+                    fan_3.setBackground(getResources().getDrawable(R.drawable.work_one_fan_2_select_corners));
+                    fan_4.setBackground(getResources().getDrawable(R.drawable.work_one_fan_2_select_corners));
+                }if (fan_flag == 5){
+                        fan_flag = 0;
+                    fan_1.setBackground(getResources().getDrawable(R.drawable.work_one_fan_1_select_corners));
+                    fan_2.setBackground(getResources().getDrawable(R.drawable.work_one_fan_2_select_corners));
+                    fan_3.setBackground(getResources().getDrawable(R.drawable.work_one_fan_2_select_corners));
+                    fan_4.setBackground(getResources().getDrawable(R.drawable.work_one_fan_2_select_corners));
+                    fan_5.setBackground(getResources().getDrawable(R.drawable.work_one_fan_3_select_corners));
+                }
+                break;
+        }
+    }
+
+    private void resetMenuState() {
+        fan_1.setBackground(getResources().getDrawable(R.drawable.work_one_fan_1_unselect_corners));
+        fan_2.setBackground(getResources().getDrawable(R.drawable.work_one_fan_2_unselect_corners));
+        fan_3.setBackground(getResources().getDrawable(R.drawable.work_one_fan_2_unselect_corners));
+        fan_4.setBackground(getResources().getDrawable(R.drawable.work_one_fan_2_unselect_corners));
+        fan_5.setBackground(getResources().getDrawable(R.drawable.work_one_fan_3_unselect_corners));
+
     }
 }
 
