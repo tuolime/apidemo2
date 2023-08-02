c11974d9e64d4ba01f8d526bc84758f9088b30f2
guojinlong <guojinlong@norinaviation.com>
2023-08-02 10:57:21 +0800

    工作一：部分逻辑-2

--- app/src/main/java/com/ss/apidemo/ui/mode/WorkSelectOneActivity.java
+++ app/src/main/java/com/ss/apidemo/ui/mode/WorkSelectOneActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.king.view.arcseekbar.ArcSeekBar;
@@ -18,7 +19,8 @@ import com.ss.apidemo.utils.PlayVoiceUtils;
 public class WorkSelectOneActivity extends BaseActivity {
 
     private TextView fan_1,fan_2,fan_3,fan_4,fan_5;
-    private TextView tv_fluence;
+    private ImageView iv_auto,iv_30,iv_100,iv_400,iv_l,iv_m,iv_f;
+    private TextView tv_fluence,tv_temprature,tv_flow,tv_total,tv_current,tv_id,tv_exact,tv_raedy;
     private ArcSeekBar arcSeekBar;
     private int fan_flag = 0;
 
@@ -38,7 +40,22 @@ public class WorkSelectOneActivity extends BaseActivity {
         fan_5 = findViewById(R.id.fan_5);
         arcSeekBar = findViewById(R.id.arcSeekBar);
         tv_fluence = findViewById(R.id.tv_fluence);
+        iv_auto = findViewById(R.id.iv_auto);
+        iv_30 = findViewById(R.id.iv_30);
+        iv_100 = findViewById(R.id.iv_100);
+        iv_400 = findViewById(R.id.iv_400);
 
+        iv_l = findViewById(R.id.iv_l);
+        iv_m = findViewById(R.id.iv_m);
+        iv_f = findViewById(R.id.iv_f);
+
+        tv_temprature = findViewById(R.id.tv_temprature);
+        tv_flow = findViewById(R.id.tv_flow);
+        tv_total = findViewById(R.id.tv_total);
+        tv_current = findViewById(R.id.tv_current);
+        tv_id = findViewById(R.id.tv_id);
+        tv_exact = findViewById(R.id.tv_exact);
+        tv_raedy = findViewById(R.id.tv_raedy);
     }
     private void initData() {
 
@@ -62,19 +79,53 @@ public class WorkSelectOneActivity extends BaseActivity {
 
     public void modeClick(View view) {
         PlayVoiceUtils.startPlayVoice(MyApplication.instance(), AppConfig.KEY);
+        resetMenuModeState();
         switch (view.getId()) {
-            case R.id.iv_save:
+            case R.id.ll_auto:
+                iv_auto.setVisibility(View.VISIBLE);
                 break;
-            case R.id.iv_setting:
+            case R.id.ll_30:
+                iv_30.setVisibility(View.VISIBLE);
                 break;
-            case R.id.iv_back:
-                finish();
+            case R.id.ll_100:
+                iv_100.setVisibility(View.VISIBLE);
                 break;
-            case R.id.iv_main:
-                startActivity(new Intent(WorkSelectOneActivity.this, SplashActivity.class));
+            case R.id.ll_400:
+                iv_400.setVisibility(View.VISIBLE);
                 break;
         }
     }
+
+    private void resetMenuModeState() {
+        iv_auto.setVisibility(View.INVISIBLE);
+        iv_30.setVisibility(View.INVISIBLE);
+        iv_100.setVisibility(View.INVISIBLE);
+        iv_400.setVisibility(View.INVISIBLE);
+    }
+
+    public void rangeClick(View view) {
+        PlayVoiceUtils.startPlayVoice(MyApplication.instance(), AppConfig.KEY);
+        resetMenuRangeState();
+        switch (view.getId()) {
+            case R.id.ll_l:
+                iv_l.setVisibility(View.VISIBLE);
+                break;
+            case R.id.ll_m:
+                iv_m.setVisibility(View.VISIBLE);
+                break;
+            case R.id.ll_f:
+                iv_f.setVisibility(View.VISIBLE);
+                break;
+
+        }
+    }
+
+    private void resetMenuRangeState() {
+        iv_l.setVisibility(View.INVISIBLE);
+        iv_m.setVisibility(View.INVISIBLE);
+        iv_f.setVisibility(View.INVISIBLE);
+    }
+
     public void selectClick(View view) {
         PlayVoiceUtils.startPlayVoice(MyApplication.instance(), AppConfig.KEY);
         resetMenuState();
